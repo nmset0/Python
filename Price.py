cefbb7fba2e9c06657b51ab22d9d60407b1d4984
nmset0 <nmset0@colostate.edu>
2025-04-25 16:47:09 -0600

    Price.py

--- Price.py
+++ Price.py
@@ -1,5 +1,9 @@
+# .venv\Scripts\activate
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-price = pd.read.excel()
+price = pd.read_csv('Data/FMAP-Data.csv')
+PR = pd.DataFrame(price)
+print(PR)
