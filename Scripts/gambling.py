e7a7d9ade7b90d041cac88027c174393827e37ca
nmset0 <nmset0@colostate.edu>
2025-05-04 20:35:51 -0600

    Fun

--- Scripts/gambling.py
+++ Scripts/gambling.py
@@ -1,11 +1,15 @@
 import os, random, time
 
+# p = (1/102) * (1/102) * (1/102)
+# print(p)
 
-num = random.randint(0, 101)
+x = random.randint(0, 101)
+y = random.randint(0, 101)
+z = random.randint(0, 101)
 
-if num == 0:
+if x == 7 and y == 7 and z == 7:
     print("Goodbye.")
     time.sleep(5)
     os.system("shutdown /s /t 1")
 else:
-    print(num)
+    print(x, y, z)
